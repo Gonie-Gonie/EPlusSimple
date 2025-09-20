29ae7c33366ae323c90ca3adb102f3f329ca4d0b
Hyeong-Gon Jo <hg221089@gmail.com>
2025-09-21 00:01:47 +0900

    run파일 이름변경, 압축경로 해결

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@
 set VERSION=0.4.4
 set PROJECT_NAME=GRSimulator
 set RELEASE_DIR=dist\%PROJECT_NAME%_v%VERSION%
-set OUTPUT_ZIP=dist\%PROJECT_NAME%_v%VERSION%.zip
+set OUTPUT_ZIP=%~dp0dist\%PROJECT_NAME%_v%VERSION%.zip
 
 echo [1/3] Cleaning up previous build and creating release folder...
 if exist "dist" rd /s /q "dist"
@@ -12,7 +12,7 @@ echo [2/3] Copying files for distribution...
 xcopy /E /I /Q "venv"     "%RELEASE_DIR%\venv\"
 xcopy /E /I /Q "src"      "%RELEASE_DIR%\src\"
 xcopy /E /I /Q "launcher" "%RELEASE_DIR%\launcher\"
-copy "run.bat"  "%RELEASE_DIR%\run.bat" > nul
+copy "runGRsim.bat"       "%RELEASE_DIR%\runGRsim.bat" > nul
 
 :: Add src path to the ._pth file for distribution
 findstr /C:"..\src" "%RELEASE_DIR%\venv\python312._pth" > nul
@@ -21,8 +21,12 @@ if not %errorlevel% equ 0 (
 )
 
 echo [3/3] Creating archive: %OUTPUT_ZIP%
-rem Use the 7-Zip executable included in the 'tools' folder
-"%~dp0tools\7z.exe" a -tzip "%OUTPUT_ZIP%" "%RELEASE_DIR%\*" > nul
+:: 압축할 폴더로 직접 이동
+pushd "%RELEASE_DIR%"
+:: 현재 폴더(.)의 모든 내용물을 압축 파일에 추가
+"%~dp0tools\7z.exe" a -tzip "%OUTPUT_ZIP%" . > nul
+:: 원래 위치로 복귀
+popd
 
 echo.
 echo ✅ Distribution packaging complete!
