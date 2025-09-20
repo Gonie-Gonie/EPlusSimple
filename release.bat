a9d5b9f12bbb5d68b68e02dc681790441d600f5a
Hyeong-Gon Jo <hg221089@gmail.com>
2025-09-20 23:52:34 +0900

    압축 구조와 runbat copy불가 오류 수정

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=1.0.0
+set VERSION=0.4.4
 set PROJECT_NAME=GRSimulator
 set RELEASE_DIR=dist\%PROJECT_NAME%_v%VERSION%
 set OUTPUT_ZIP=dist\%PROJECT_NAME%_v%VERSION%.zip
@@ -12,17 +12,17 @@ echo [2/3] Copying files for distribution...
 xcopy /E /I /Q "venv"     "%RELEASE_DIR%\venv\"
 xcopy /E /I /Q "src"      "%RELEASE_DIR%\src\"
 xcopy /E /I /Q "launcher" "%RELEASE_DIR%\launcher\"
-xcopy /E /I /Q "run.bat"  "%RELEASE_DIR%\run.bat"
+copy "run.bat"  "%RELEASE_DIR%\run.bat" > nul
 
 :: Add src path to the ._pth file for distribution
-findstr /C:"..\src" "%RELEASE_DIR%\python-portable\python312._pth" > nul
+findstr /C:"..\src" "%RELEASE_DIR%\venv\python312._pth" > nul
 if not %errorlevel% equ 0 (
-    echo ..\src >> "%RELEASE_DIR%\python-portable\python312._pth"
+    echo ..\src >> "%RELEASE_DIR%\venv\python312._pth"
 )
 
 echo [3/3] Creating archive: %OUTPUT_ZIP%
 rem Use the 7-Zip executable included in the 'tools' folder
-"%~dp0tools\7z.exe" a -tzip "%OUTPUT_ZIP%" "%RELEASE_DIR%" > nul
+"%~dp0tools\7z.exe" a -tzip "%OUTPUT_ZIP%" "%RELEASE_DIR%\*" > nul
 
 echo.
 echo ✅ Distribution packaging complete!
