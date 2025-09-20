3519a6e605f040c46f0a79b74aac58323b134736
Hyeong-Gon Jo <hg221089@gmail.com>
2025-09-20 19:13:35 +0900

    또 오타수정

--- scripts/run_grexcel.py
+++ scripts/run_grexcel.py
@@ -8,7 +8,6 @@ from pyGRsim import run_grjson, run_grexcel
 target_dir   = r"B:\공유 드라이브\01 진행과제\(안전원) 시뮬레이터\12 개발\scripts\run_grexcel"
 
 target_files = [
-    r"063_지암보건진료소_GR 이전_preprocess.xlsx",
     r"제물포어린이집_v250721retrofit.xlsx",
     r"제물포어린이집_v250721.xlsx",
 ]
