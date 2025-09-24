5f4d254a3845571a53adfd6f8ffd2eeddb75a1dd
Hyeong-Gon Jo <hg221089@gmail.com>
2025-09-24 10:41:16 +0900

    작업중~

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from pyGRsim import run_grexcel
 from pyGRsim.reb.preprocess import process_excel_file
@@ -6,10 +7,10 @@ from pyGRsim.debug import debug_excel, report_result
 
 working_dir   = r"B:\공유 드라이브\01 진행과제\(안전원) 시뮬레이터\12 개발\tests\250924 PHIKO excel test"
 
-target_files = [file for file in os.listdir(working_dir) if file.endswith(r".xlsx")]
+target_files = [file for file in os.listdir(working_dir) if file.endswith(r".xlsx") and not file.endswith(r"preprocess.xlsx")]
 
 target_files = [
-    r"054_상남보건지소_GR이전.xlsx"
+    r"022_화송어린이집_GR이전.xlsx"
 ]
 
 for file in target_files:
@@ -23,9 +24,13 @@ for file in target_files:
     if len(errors) > 1:
         continue
     
-    run_grexcel(
+    output_filepath = run_grexcel(
         processed_file,
         fullfilename.replace(r".xlsx",r".grr")
     )
 
-pass
+    with open(output_filepath, "r") as f:
+        data = json.load(f)
+    
+    if sum(sum(data["site_uses"]["cooling"].values(),[])) < 0.1:
+        print(f"🐲🐲🐲🐲🐲🐲🐲🐲🐲🐲  {file}  🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
