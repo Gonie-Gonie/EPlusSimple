55175d4507199584772f386dea9a2ebf9f93e7f0
Hyeong-Gon Jo <hg221089@gmail.com>
2025-10-02 13:34:48 +0900

    scipt 구조잡기

---
+++ scripts/run_REB_excel.py
@@ -0,0 +1,25 @@
+
+# ------------------------------------------------------------------------ #
+#                                  MODULES                                 #
+# ------------------------------------------------------------------------ #
+
+# built-in modules
+import os
+
+# third-party modules
+
+
+# local modules
+from reb import run_rebexcel
+
+
+# setting
+working_dir = r"B:\공유 드라이브\01 진행과제\(안전원) 시뮬레이터\12 개발\scripts\run_REB_excel"
+filelist = [file for file in os.listdir(working_dir) if file.endswith(".xlsx")]
+
+
+if __name__ == "__main__":
+    
+    for file in filelist:
+        run_rebexcel(file)
+
---
+++ src/reb/api.py
@@ -0,0 +1,42 @@
+
+# ------------------------------------------------------------------------ #
+#                                  MODULES                                 #
+# ------------------------------------------------------------------------ #
+
+# built-in modules
+
+
+# third-party modules
+
+
+# local modules
+from idragon import IDF
+
+
+def rebexcel_to_idf(
+    input_filepath:str,
+    ) -> IDF:
+
+    return
+
+def run_rebexcel(
+    input_filepath :str|None,
+    output_filepath:str|None=None,
+    *,
+    save           :bool    =True,
+    ) -> str:
+    
+    
+    # preprocess
+    
+    # read excel to model
+    
+    # read excel to survey
+    
+    # run
+    
+    # save if required
+    
+    # remove unused files
+    
+    return
