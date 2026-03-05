1bb7057ba3fcb2bda66456d232ae561de524eb0d
Hyeong-Gon Jo <hg221089@gmail.com>
2026-03-05 18:07:52 +0900

    TRM 3장 수정중

--- runEngine.bat
+++ runEngine.bat
@@ -14,18 +14,11 @@ SET "LOG_FILE=%BATCH_DIR%log.log"
 
 REM --- Delete existing log file ---
 IF EXIST "%LOG_FILE%" (
-    ECHO Deleting existing log file: %LOG_FILE%
     DEL "%LOG_FILE%"
 )
-
-ECHO.
-ECHO Executing Python script...
-ECHO Log will be saved to: %LOG_FILE%
-ECHO.
 
 REM --- Execute Python script and save log ---
 REM Pass all arguments and redirect both stdout and stderr to the log file
 "%PYTHON_EXE%" -m "%MODULE_NAME%" %* > "%LOG_FILE%" 2>&1
 
-ECHO.
 ECHO Script execution finished.
